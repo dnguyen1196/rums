060f3021d2e2b729968ddd356162415a02963265
Duc Nguyen <dnguyen1196.github@gmail.com>
2022-08-05 16:53:46 -0400

    added bayesian method

--- rums/algorithms/mixtures/__init__.py
+++ rums/algorithms/mixtures/__init__.py
@@ -2,3 +2,4 @@ from rums.algorithms.mixtures.spectral_em_pl import SpectralEM
 from rums.algorithms.mixtures.spectral_em_cml import SpectralSurrogateEM, EM_CML
 from rums.algorithms.mixtures.em_gmm import EM_GMM_PL
 from rums.algorithms.mixtures.emm import EMM, MM
+from rums.algorithms.mixtures.bayesian_em_gs import BayesianEMGS
